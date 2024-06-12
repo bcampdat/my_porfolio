de3081ba3a25e2164a8a0f3abfb2b499ea1c740f
Bea <bcampdat@gmail.com>
2024-06-12 21:55:02 +0200

    routes y uso Navlink

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PorfolioContainer from './porfolio/porfolio-container';
-import NavigationContainer from './navigation/navigation-container';
+import PorfolioContainer from "./porfolio/porfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <NavigationContainer />
+      <div className="app">       
+
+        <Router>
+          <div>
+
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              
+            </Switch>
+
+          </div>
+        </Router>
+
         {/* <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2> */}
         <h1>My_Porfolio</h1>
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { NavLink } from 'react-router-dom';
+
 export default class NavigationContainer extends Component {
     constructor() {
         super();
@@ -8,10 +10,18 @@ export default class NavigationContainer extends Component {
     render() {
         return (
             <div>
-            <button>Home</button>
-            <button>About</button>
-            <button>Contact</button>
-            <button>Blog</button>
+            
+            <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+            <NavLink to="/about" activeClassName="nav-link-active">About</NavLink>
+
+            {/* <a href="./">Wrong Home</a> */}
+            {/* sin dinamismo cargaria la pag entera y perderia la funcionalidad */}
+
+            <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+            <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+
+            {/* <button>Contact</button>
+            <button>Blog</button> */}
             {/* depende del rol aparecera el boton o no */}
             {/* {true ? <button>Add Blog</button> : null} */}
             {false ? <button>Add Blog</button> : null}
