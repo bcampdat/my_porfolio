f5710296c7ef087be4d59321d877eed619c16780
Bea <bcampdat@gmail.com>
2024-06-18 19:54:44 +0200

    de componente a funcional

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,48 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { NavLink } from "react-router-dom";
 
-export default class NavigationContainer extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="nav-wrapper">
-        <div className="left-side">
-          <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="nav-link-active">
-              Home
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/about-me" activeClassName="nav-link-active">
-              About
-            </NavLink>
-          </div>
-          {/* <a href="./">Wrong Home</a> */}
-          {/* sin dinamismo cargaria la pag entera y perderia la funcionalidad */}
-          <div className="nav-link-wrapper">
-            <NavLink to="/contact" activeClassName="nav-link-active">
-              Contact
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/blog" activeClassName="nav-link-active">
-              Blog
-            </NavLink>
-          </div>
-          {/* <button>Contact</button>
-            <button>Blog</button> */}
-          {/* depende del rol aparecera el boton o no */}
-          {/* {true ? <button>Add Blog</button> : null} */}
-          {/*  Tendremos en cuenta mas adelante */}
-          {/* {false ? <button>Add Blog</button> : null} */}
+const NavigationContainer = (props) => {
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
         </div>
-        <div className="left-side">BCAMPDAT</div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+        </div>
+        {/* <a href="./">Wrong Home</a> */}
+        {/* sin dinamismo cargaria la pag entera y perderia la funcionalidad */}
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+        </div>
+        {/* <button>Contact</button>
+            <button>Blog</button> */}
+        {/* depende del rol aparecera el boton o no */}
+        {/* {true ? <button>Add Blog</button> : null} */}
+        {/*  Tendremos en cuenta mas adelante */}
+        {/* {false ? <button>Add Blog</button> : null} */}
       </div>
-    );
-  }
-}
+      <div className="left-side">BCAMPDAT</div>
+    </div>
+  );
+};
+
+export default NavigationContainer;
