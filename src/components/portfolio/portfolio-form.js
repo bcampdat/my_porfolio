34ccadd63af41450dedb429004112ce6f3cc11f5
Bea <bcampdat@gmail.com>
2024-06-23 20:47:36 +0200

    Dropzone-componet  instalacion y uso en el formulario

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
+import DropzoneComponent from "react-dropzone-component";
+
+import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
+import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
 export default class PortfolioForm extends Component {
   constructor(props) {
@@ -19,8 +23,67 @@ export default class PortfolioForm extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.componentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+
+    this.handleThumbDrop = this.handleThumbDrop.bind(this);
+
+    this.handleBannerDrop = this.handleBannerDrop.bind(this);
+
+    this.handleLogoDrop = this.handleLogoDrop.bind(this);
+
   }
 
+  handleThumbDrop() {
+    return {
+      // addicion del archivo con DropzoneComponent
+      addedfile: (file) => {
+        this.setState({
+          thumb_image: file,
+        });
+      },
+    };
+  }
+
+  handleBannerDrop() {
+    return {
+      // addicion del archivo con DropzoneComponent
+      addedfile: (file) => {
+        this.setState({
+          banner_image: file,
+        });
+      },
+    };
+  }
+
+  handleLogoDrop() {
+    return {
+      // addicion del archivo con DropzoneComponent
+      addedfile: (file) => {
+        this.setState({
+          logo: file,
+        });
+      },
+    };
+  }
+
+  componentConfig() {
+    return {
+      iconFiletypes: [".jpg", ".png"],
+      showFiletypeIcon: true,
+      postUrl: "https://httpbin.org/post",
+      // siempre retornara true
+      // siempre nos permitira llamarla con verbosHttp
+    };
+  }
+
+  djsConfig() {
+    return {
+      addRemoveLinks: true,
+      maxFiles: 1,
+    };
+  }
   buildForm() {
     let formData = new FormData();
     // creamos el objeto para el formulario
@@ -30,6 +93,18 @@ export default class PortfolioForm extends Component {
     formData.append("portfolio_item[category]", this.state.category);
     formData.append("portfolio_item[position]", this.state.position);
 
+    if (this.state.thumb_image) {
+      // especialmente para imagenes en el formulario
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+    }
+    if (this.state.banner_image) {
+      // especialmente para imagenes en el formulario
+      formData.append("portfolio_item[banner_image]", this.state.banner_image);
+    }
+    if (this.state.logo) {
+      // especialmente para imagenes en el formulario
+      formData.append("portfolio_item[logo]", this.state.logo);
+    }
     // debugger;
     return formData;
   }
@@ -60,10 +135,9 @@ export default class PortfolioForm extends Component {
         console.log("portfolio error", error);
       }),
       event.preventDefault();
-       // eventos sintenticos dom virtual
+    // eventos sintenticos dom virtual
   }
 
- 
   render() {
     return (
       <div>
@@ -97,19 +171,18 @@ export default class PortfolioForm extends Component {
             />
 
             {/* <input */}
-            <select            
+            <select
               // type="text"
               name="category"
               // placeholder="Category"
               value={this.state.category}
               onChange={this.handleChange}
-            // />
+              // />
             >
               <option value="eComerce">eComerce</option>
               <option value="Scheduling">Scheduling</option>
               <option value="Enterprise">Enterprise</option>
             </select>
-
           </div>
 
           <div>
@@ -123,6 +196,28 @@ export default class PortfolioForm extends Component {
             />
           </div>
 
+          <div className="image-uploaders">
+            {" "}
+            {/* para subir imagenes */}
+            <DropzoneComponent
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleThumbDrop()}
+            />
+
+            <DropzoneComponent
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleBannerDrop()}
+            />
+
+            <DropzoneComponent
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleLogoDrop()}
+            />
+
+          </div>
           <div>
             <button type="submit">Save</button>
           </div>
