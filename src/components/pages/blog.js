5e7172b1cfa4fd6f09356d3e3e883326f7d4cfd3
Bea <bcampdat@gmail.com>
2024-06-29 18:56:48 +0200

    .

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,59 @@
-import React from "react";
+// import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function () {
-  return (
-    <div>
-      <h2>Blog</h2>
+// export default function () {
+//   return (
+//     <div>
+//       <h2>Blog</h2>
 
+//       <div>
+//         <Link to="/about-me">Read more myself</Link>
+//       </div>
+//     </div>
+//   );
+// }
+class Blog extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://bcampdat.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    return (
       <div>
-        <Link to="/about-me">Read more myself</Link>
+        <h2>Blog</h2>
+
+        <div>
+          <Link to="/about-me">Read more about myself</Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog;
