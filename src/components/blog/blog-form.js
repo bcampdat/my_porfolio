e1526c3876708ac34fdd3acbbf8728ac6c251a2a
Bea <bcampdat@gmail.com>
2024-07-03 18:50:33 +0200

    instalacion e inicio    draft

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import axios from "axios";
 
+import RichTextEditor from "../forms/rich-text-editor";
+
 export default class BlogForm extends Component {
   constructor(props) {
     super(props);
@@ -9,10 +11,20 @@ export default class BlogForm extends Component {
     this.state = {
       title: "",
       blog_status: "",
+
+      content: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+  }
+
+  handleRichTextEditorChange(content) {
+    this.setState({
+      // content: content  lo mismo que lo de abajo      js moderno
+      content,
+    });
   }
 
   buildForm() {
@@ -20,6 +32,7 @@ export default class BlogForm extends Component {
 
     formData.append("portfolio_blog[title]", this.state.title);
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+    formData.append("portfolio_blog[content]", this.state.content);
 
     return formData;
   }
@@ -81,6 +94,14 @@ export default class BlogForm extends Component {
             value={this.state.blog_status}
           />
         </div>
+
+        <div className="one-column">
+          {/* <RichTextEditor /> */}
+          <RichTextEditor
+            handleRichTextEditorChange={this.handleRichTextEditorChange}
+          />
+        </div>
+
         <button className="btn">Save</button>
       </form>
     );
