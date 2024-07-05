b27948a481fd2dbf60515a5b0950c63e42c8188e
Bea <bcampdat@gmail.com>
2024-07-05 19:58:05 +0200

    image add  in rich text

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -13,27 +13,47 @@ export default class RichTextEditor extends Component {
     };
 
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.getBase64 = this.getBase64.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
   }
 
   onEditorStateChange(editorState) {
-    // this.setState({    
+    // this.setState({
     //   editorState,
     // })
     // this.props.handleRichTextEditorChange(editorState);
-//  }
-     //   no funcionaria en este caso al ser setState asincrono  
-     // no se renderiza la actualizacion del  estado a tiempo y pierde memoria
+    //  }
+    //   no funcionaria en este caso al ser setState asincrono
+    // no se renderiza la actualizacion del  estado a tiempo y pierde memoria
 
-    // mejor escribirlo de esta forma 
+    // mejor escribirlo de esta forma
     this.setState(
       { editorState },
       this.props.handleRichTextEditorChange(
         draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
-        // convertimos content en html   tags usables 
+        // convertimos content en html   tags usables
         // ej   <p>draft editor rich<strong>draft editor rich</strong></p>
       )
     );
   }
+
+  // convertivos la imagen en base64 para poder trabajar con ella
+  
+
+  getBase64(file, callback) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callback(reader.result);
+    reader.onerror = (error) => {};
+  }
+
+  uploadFile(file) {
+    console.log("upload file", file);
+    return new Promise((resolve, reject) => {
+      this.getBase64(file, (data) => resolve({ data: { link: data } }));
+    });
+  }
+
   render() {
     return (
       <div>
@@ -44,6 +64,19 @@ export default class RichTextEditor extends Component {
           wrapperClassName="demo-wrapper"
           editorClassname="demo-editor"
           onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadFile,
+              alt: { present: true, mandatory: false },
+              previewImage: true,
+              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg",
+            },
+          }}
         />
       </div>
     );
