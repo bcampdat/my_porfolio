26dbed71ee3bea36c6a35d79f31224833e971990
Bea <bcampdat@gmail.com>
2024-07-11 21:23:32 +0200

    details style

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -15,6 +15,10 @@ import axios from "axios";
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      portfolioItem: {},
+    };
   }
 
   componentWillMount() {
@@ -28,7 +32,10 @@ export default class PortfolioDetail extends Component {
         { withCredentials: true }
       )
       .then((response) => {
-        console.log("res", response);
+        // console.log("res", response);
+        this.setState({
+          portfolioItem: response.data.portfolio_item,
+        });
       })
       .catch((error) => {
         console.log("getportfolioitem error", error);
@@ -36,9 +43,48 @@ export default class PortfolioDetail extends Component {
   }
 
   render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url,
+    } = this.state.portfolioItem;
+
+    
+    const bannerStyles = {
+      backgroundImage: "url(" + banner_image_url + ")",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center",
+    };
+
+    const logoStyles = {
+      width: "200px",
+    };
+
     return (
-      <div>
-        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      <div className="portfolio-detail-wrapper">
+
+        {/* <h2>Portfolio Detail for {this.props.match.params.slug}</h2> */}
+        {/* <h2>{name}</h2>
+        <p>{description}</p> */}
+
+        <div className="banner" style={bannerStyles}>
+          <img src={logo_url} style={logoStyles} />
+        </div>
+
+        <div className="portfolio-detail-description-wrapper">
+          <div className="description">{description}</div>
+        </div>
+
+        <div className="bottom-content-wrapper">
+          <a href={url} className="site-link" target="_blank">
+            Visit {name}
+          </a>
+        </div>
       </div>
     );
   }
